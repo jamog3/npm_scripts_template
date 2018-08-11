a0678a8e6bd628832338820ed35ce1905ce5a01d
jamog3 <mailtojam@gmail.com>
2018-08-11 16:15:17 +0900

    Add image min

--- .spritesmith.js
+++ .spritesmith.js
@@ -1,7 +1,7 @@
 module.exports = {
   src: 'src/images/_sprites/*.png', // これらの画像を1個にまとめる。
-  destImage: 'src/images/sprite.png', // 生成される画像
-  destCSS: 'src/stylesheets/_partial/_sprite.sass', // 生成されるCSS
+  destImage: 'src/images/sprites.png', // 生成される画像
+  destCSS: 'src/stylesheets/_partial/_sprites.sass', // 生成されるCSS
   algorithm: 'binary-tree', // 並び方
   padding: 2
 };
---
+++ rollup.config.build.js
@@ -0,0 +1,20 @@
+import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+
+module.exports = {
+  input: 'src/javascripts/common.js',
+  output: {
+    file: 'build/assets/javascripts/common.js',
+    format: 'iife',
+    sourcemap: false
+  },
+  plugins: [
+    babel(babelrc()),
+    resolve(),
+    commonjs({
+      include: 'node_modules/**'
+    })
+  ]
+}
