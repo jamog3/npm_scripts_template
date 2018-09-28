fc79c26dc28cf5ab1689d084bfe8dc1fa9246715
jamog3 <mailtojam@gmail.com>
2018-09-28 17:54:52 +0900

    Update sprite css template

--- config/spritesmith.js
+++ config/spritesmith.js
@@ -3,5 +3,6 @@ module.exports = {
   destImage: 'src/images/sprites.png', // 生成される画像
   destCSS: 'src/stylesheets/_partial/_sprites.sass', // 生成されるCSS
   algorithm: 'binary-tree', // 並び方
-  padding: 2
+  padding: 2,
+  cssTemplate: 'config/spritesmith.sass.template.mustache'
 };
