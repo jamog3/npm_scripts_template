62baa59344840d71434a187e597b8de49dc358dc
jamog3 <mailtojam@gmail.com>
2018-11-20 14:02:42 +0900

    Update js uglify

--- config/rollup.config.build.js
+++ config/rollup.config.build.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
 
 module.exports = {
@@ -15,6 +16,12 @@ module.exports = {
     resolve(),
     commonjs({
       include: 'node_modules/**'
+    }),
+    uglify({
+      output: {
+        // /*! で始まる License Comment を残す
+        comments: /^!/
+      }
     })
   ]
-}
+};
