9822779174ce474007cea28f3ec42f1a218e7dc0
jamog3 <mailtojam@gmail.com>
2022-10-01 16:48:01 +0900

    fix: remove vue

--- config/rollup.config.build.js
+++ config/rollup.config.build.js
@@ -1,10 +1,8 @@
-import alias from '@rollup/plugin-alias';
 import commonjs from 'rollup-plugin-commonjs';
 import multiInput from 'rollup-plugin-multi-input';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import vuePlugin from 'rollup-plugin-vue';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -17,19 +15,9 @@ export default {
     plugins: [terser()],
   },
   plugins: [
-    alias({
-      entries: {
-        vue: 'node_modules/vue/dist/vue.esm-bundler.js',
-      },
-    }),
     multiInput(),
-    vuePlugin({
-      target: 'browser',
-    }),
     replace({
       'process.env.NODE_ENV': JSON.stringify('production'),
-      __VUE_OPTIONS_API__: JSON.stringify(true),
-      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
     }),
     commonjs({
       include: 'node_modules/**',
--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,10 +1,8 @@
-import alias from '@rollup/plugin-alias';
 import commonjs from 'rollup-plugin-commonjs';
 import multiInput from 'rollup-plugin-multi-input';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import vuePlugin from 'rollup-plugin-vue';
 
 export default {
   input: ['src/javascripts/*.ts'],
@@ -15,19 +13,9 @@ export default {
     sourcemap: true,
   },
   plugins: [
-    alias({
-      entries: {
-        vue: 'node_modules/vue/dist/vue.esm-bundler.js',
-      },
-    }),
     multiInput(),
-    vuePlugin({
-      target: 'browser',
-    }),
     replace({
       'process.env.NODE_ENV': JSON.stringify('development'),
-      __VUE_OPTIONS_API__: JSON.stringify(true),
-      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
     }),
     commonjs({
       include: 'node_modules/**',
